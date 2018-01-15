4736451a88363da8cc2b0753d2e19e09196adb48
NLag <NLag@users.noreply.github.com>
2018-01-16 02:14:53 +0700

    make file transfer from client to server

---
+++ tcp/file/client_file.c
@@ -0,0 +1,100 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+int main(int argc, char* argv[]) {
+    int so,numread,numwrite;
+    struct sockaddr_in ad;
+    char stt[100];
+    char filename[200];
+    strcpy(filename,argv[2]);
+    FILE *pfile;
+    long filelen;
+    char filelenstr[200];
+    char *buffer;
+
+    socklen_t ad_length = sizeof(ad);
+    struct hostent *hep;
+
+    // create socket
+    int serv = socket(AF_INET, SOCK_STREAM, 0);
+
+    // init address
+    hep = gethostbyname(argv[1]);
+    memset(&ad, 0, sizeof(ad));
+    ad.sin_family = AF_INET;
+    ad.sin_addr = *(struct in_addr *)hep->h_addr_list[0];
+    ad.sin_port = htons(12345);
+
+    // connect to server
+    connect(serv, (struct sockaddr *)&ad, ad_length);
+    printf("connect server success\n");
+
+
+    //send file name
+    printf("send file name to server: %s\n",filename);
+    numwrite = write(serv, filename, strlen(filename) + 1);
+    if (numwrite <= 0)
+    {
+        printf("Filename not sended: %d\n", numwrite);
+
+        fclose(pfile);
+        close(serv);
+        return 0;
+    }
+
+    read(serv, stt, sizeof(stt));
+
+    //open file
+    pfile = fopen(filename, "rb");
+    //go to end
+    fseek(pfile, 0, SEEK_END);
+    //check the number of bytes = position of pointer
+    filelen = ftell(pfile);
+    //go back to start
+    rewind(pfile);
+    //long to string
+    sprintf(filelenstr,"%ld",filelen);
+
+    //send file size
+    printf("send file size to server: %s\n",filelenstr);
+    numwrite = write(serv, filelenstr, strlen(filelenstr) + 1);
+    if (numwrite <= 0)
+    {
+        printf("File size not sended: %d\n",numwrite);
+        fclose(pfile);
+        close(serv);
+        return 0;
+    }
+
+    read(serv, stt, sizeof(stt));
+
+    //create free memory for file
+    buffer = (char *)malloc((filelen + 1) * sizeof(char));
+    //read file into memory buffer
+    fread(buffer, filelen, 1, pfile);
+
+    //send file to server
+    printf("send file to server\n");
+    numwrite = write(serv, buffer, strlen(buffer) + 1);
+    if (numwrite <= 0)
+    {
+        printf("File size not sended: %d\n",numwrite);
+        free(buffer);
+        fclose(pfile);
+        close(serv);
+        return 0;
+    }
+    printf("send complete\n");
+
+    read(serv, stt, sizeof(stt));
+
+    free(buffer);
+    fclose(pfile);
+    close(serv);
+    return 0;
+}
---
+++ tcp/file/server_file.c
@@ -0,0 +1,104 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+int main() {
+    int ss, cli, pid, numread, numwrite;
+    struct sockaddr_in ad;
+    socklen_t ad_length = sizeof(ad);
+    char stt[100] = "OK";
+    char filename[200];
+    FILE *pfile;
+    long filelen;
+    char filelenstr[200];
+    char *buffer;
+
+    // create the socket
+    ss = socket(AF_INET, SOCK_STREAM, 0);
+
+    // bind the socket to port 12345
+    memset(&ad, 0, sizeof(ad));
+    ad.sin_family = AF_INET;
+    ad.sin_addr.s_addr = INADDR_ANY;
+    ad.sin_port = htons(12345);
+    bind(ss, (struct sockaddr *)&ad, ad_length);
+
+    // then listen
+    listen(ss, 0);
+    printf("server init completed\n");
+
+    while (1) {
+        // an incoming connection
+        cli = accept(ss, (struct sockaddr *)&ad, &ad_length);
+
+        pid = fork();
+        if (pid == 0) {
+            // I'm the son, I'll serve this client
+            printf("client connected\n");
+            
+            // receive file name
+            numread = read(cli, filename, sizeof(filename));
+            if (numread <= 0)
+            {
+                printf("Read file name failed: %d \n", numread);
+                close(cli);
+                return 0;
+            }
+            printf("File name received: %s\n",filename);
+            write(cli, stt, strlen(stt) + 1);
+
+            // receive file size
+            numread = read(cli, filelenstr, sizeof(filelenstr));
+            if (numread <= 0)
+            {
+                printf("Read file size failed: %d \n", numread);
+                close(cli);
+                return 0;
+            }
+            printf("File size received: %s\n",filelenstr);
+            filelen = atol(filelenstr);
+            write(cli, stt, strlen(stt) + 1);
+
+            // receive file 
+            buffer = (char *)malloc((filelen + 1) * sizeof(char));
+            numread = read(cli, buffer, sizeof(buffer));
+            if (numread <= 0)
+            {
+                printf("Read file failed: %d \n", numread);
+                free(buffer);
+                close(cli);
+                return 0;
+            }
+            printf("File received.\n");
+            write(cli, stt, strlen(stt) + 1);
+
+            pfile = fopen(filename, "wb");
+            int written = fwrite(buffer, sizeof(char), filelen, pfile);
+            if (written < 1)
+            {
+                printf("Can't save to file");
+                free(buffer);
+                fclose(pfile);
+                close(cli);
+                return 0;
+            }
+
+            printf("File saved\n");
+
+            free(buffer);
+            fclose(pfile);
+            close(cli);
+            return 0;
+        }
+        else {
+            // I'm the father, continue the loop to accept more clients
+            continue;
+        }
+    }
+    // disconnect
+    close(cli);
+}
