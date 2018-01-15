73fe18c20d05db53f18b931df6a4d650b9a6bca7
NLag <NLag@users.noreply.github.com>
2018-01-16 00:32:13 +0700

    recover default chat client and server, move continuous chat to different folder

--- tcp/chat_client.c
+++ tcp/chat_client.c
@@ -7,7 +7,7 @@
 #include <netdb.h>
 
 int main(int argc, char* argv[]) {
-    int so,pid;
+    int so;
     char s[100];
     struct sockaddr_in ad;
 
@@ -26,21 +26,19 @@ int main(int argc, char* argv[]) {
 
     // connect to server
     connect(serv, (struct sockaddr *)&ad, ad_length);
-    // after connected, make client thread to receive mess from server
-    pid = fork();
-    if (pid == 0)
-    {   
-        while(1){
-            read(serv, s, sizeof(s));
-            printf("server says: %s\n", s);
-        }
-        return 0;
-    }
+
     while (1) {
+        // after connected, it's client turn to chat
+
         // send some data to server
+        printf("client>");
         scanf("%s", s);
         write(serv, s, strlen(s) + 1);
+
+        // then it's server turn
+        read(serv, s, sizeof(s));
+
+        printf("server says: %s\n", s);
     }
-    return 0;
 }
 
--- tcp/chat_server.c
+++ tcp/chat_server.c
@@ -24,38 +24,30 @@ int main() {
 
     // then listen
     listen(ss, 0);
-    printf("Server init complete\n");
+
     while (1) {
         // an incoming connection
         cli = accept(ss, (struct sockaddr *)&ad, &ad_length);
-        printf("client %d connected\n",cli);
-        // create thread to receive from client
+
         pid = fork();
         if (pid == 0) {
-            // I'm the son, I'll serve receive from this client
+            // I'm the son, I'll serve this client
+            printf("client connected\n");
             while (1) {
-                // I wait and read message from client
+                // it's client turn to chat, I wait and read message from client
                 read(cli, s, sizeof(s));
-                printf("client %d says: %s\n",cli,s);
+                printf("client says: %s\n",s);
+
+                // now it's my (server) turn
+                printf("server> ");
+                scanf("%s", s);
+                write(cli, s, strlen(s) + 1);
             }
             return 0;
-        } else {
-            // If the father, create thread to send to client
-            pid = fork();
-            if (pid == 0)
-            {   
-                // I'm the son, I'll serve send to this client
-                while (1) {
-                    // now it's my (server) turn
-                    // keep read mess and send to client
-                    scanf("%s", s);
-                    write(cli, s, strlen(s) + 1);
-                }
-                return 0;
-            } else {
-                // I'm the father, continue the loop to accept more clients
-                continue;
-            } 
+        }
+        else {
+            // I'm the father, continue the loop to accept more clients
+            continue;
         }
     }
     // disconnect
