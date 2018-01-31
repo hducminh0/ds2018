9cbe58efabfa6ec6c5a4f4de18f2d7a888a3b249
NLag <NLag@users.noreply.github.com>
2018-01-31 14:54:09 +0700

    fix indent

--- Labwork 2/rpc/fileclient.c
+++ Labwork 2/rpc/fileclient.c
@@ -19,71 +19,71 @@ int main(int argc, char const *argv[])
 	CLIENT *cl;
 	cl=clnt_create(host,FILETRANSFER_PROG,FILETRANSFER_VERS,"tcp");
 	if (cl == NULL) {
-        /*
-         * Couldn't establish connection with server.
-         * Print error message and stop.
-         */
-         clnt_pcreateerror(host);
-         exit(1);
-    }
-    /* Get filename. */
-    char *name = argv[2];
+		/*
+		 * Couldn't establish connection with server.
+		 * Print error message and stop.
+		 */
+		 clnt_pcreateerror(host);
+		 exit(1);
+	}
+	/* Get filename. */
+	char *name = argv[2];
 
-    FILE *file;
-    file = fopen(name, "r");
+	FILE *file;
+	file = fopen(name, "r");
 
-    /* 
-     * Create buffer 
-     * Change file name that save on server
-     * ex: abc.txt will be save as abc.txtsv
-     */
-    buffer buff;
-    buff.name = strcat(name,"sv");
+	/* 
+	 * Create buffer 
+	 * Change file name that save on server
+	 * ex: abc.txt will be save as abc.txtsv
+	 */
+	buffer buff;
+	buff.name = strcat(name,"sv");
 
-    int read_bytes;
-    int *result;
+	int read_bytes;
+	int *result;
   	long total_bytes = 0;
-    
-    /* Call the remote procedure readdir on the server */
-    while (1) {
-    	/* Read a BLOCK of bytes from file to data. */
-        read_bytes = fread(buff.data, 1, BLOCK, file);
-        total_bytes += read_bytes;
-        buff.numbytes = read_bytes;
-        result = filetransfer_proc_1(&buff, cl);
+	
+	/* Call the remote procedure readdir on the server */
+	while (1) {
+		/* Read a BLOCK of bytes from file to data. */
+		read_bytes = fread(buff.data, 1, BLOCK, file);
+		total_bytes += read_bytes;
+		buff.numbytes = read_bytes;
+		result = filetransfer_proc_1(&buff, cl);
 
-        if (result == NULL) {
-            /*
-             * An RPC error occurred while calling the server.
-             * Print error message and stop.
-             */
-            clnt_perror(cl, host);
-            exit(1);
-        }
+		if (result == NULL) {
+			/*
+			 * An RPC error occurred while calling the server.
+			 * Print error message and stop.
+			 */
+			clnt_perror(cl, host);
+			exit(1);
+		}
 
-        /* Successfully called the remote procedure. */
-        if (*result == 0) {
-            /*
-             * A remote system error occurred.
-             * Print error message and stop.
-             */
-            errno = *result;
-            perror(name);
-            exit(1);
-        }
+		/* Successfully called the remote procedure. */
+		if (*result == 0) {
+			/*
+			 * A remote system error occurred.
+			 * Print error message and stop.
+			 */
+			errno = *result;
+			perror(name);
+			exit(1);
+		}
 
-        /*
-         * Successfully got a block of the file.
-         * Write into our local file.
-         */
-        if (read_bytes < BLOCK) 
-            break;
-    }
+		/*
+		 * Successfully got a block of the file.
+		 * Write into our local file.
+		 */
+		if (read_bytes < BLOCK) 
+			break;
+	}
 
-    fclose(file);
+	fclose(file);
 
-    printf("File Sent: %s\n",buff.name );
-    printf("Bytes Sent: %ld\n", total_bytes);
+	printf("File Sent: %s\n",buff.name );
+	printf("Bytes Sent: %ld\n", total_bytes);
 
 	return 0;
 }
--- Labwork 2/rpc/fileserver.c
+++ Labwork 2/rpc/fileserver.c
@@ -7,16 +7,16 @@
 
 int* filetransfer_proc_1_svc(buffer * recvbuff, struct svc_req *rqstp) {
 	FILE *file;
-    static int write_bytes;
-    
-    file = fopen(recvbuff->name, "a");
-    if (file == NULL) {
-        write_bytes = -1;
-        return &write_bytes;
-    }
+	static int write_bytes;
+	
+	file = fopen(recvbuff->name, "a");
+	if (file == NULL) {
+		write_bytes = -1;
+		return &write_bytes;
+	}
 
-    write_bytes = fwrite(recvbuff->data, 1, recvbuff->numbytes, file);
-    fclose(file);
+	write_bytes = fwrite(recvbuff->data, 1, recvbuff->numbytes, file);
+	fclose(file);
 
-    return &write_bytes;
+	return &write_bytes;
 }
