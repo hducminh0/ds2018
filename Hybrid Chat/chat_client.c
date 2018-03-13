766a7dac9ff60a4ed3c918dae01882c5cc249782
NLag <NLag@users.noreply.github.com>
2018-03-13 10:05:03 +0700

    add login sequence client

--- Hybrid Chat/chat_client.c
+++ Hybrid Chat/chat_client.c
@@ -8,13 +8,9 @@
 #include <signal.h>
 #include <netinet/in.h>
 
-void *get_in_addr(struct sockaddr *sv_address)
-{
-    return &(((struct sockaddr_in*)sv_address)->sin_addr);
-}
-
 int main(int argc, char* argv[]) {
 //----------------------------------INIT CLIENT Connection to SERVER----------------------------------
+    char mess[100];
     int serv;
     struct sockaddr_in sv_address;
 
@@ -32,8 +28,49 @@ int main(int argc, char* argv[]) {
     sv_address.sin_port = htons(12345);
 
     // connect to server
-    connect(serv, (struct sockaddr *)&sv_address, ad_length);
-    // after connected, make client thread to send mess to server
+    int connect_status = connect(serv, (struct sockaddr *)&sv_address, ad_length);
+    if (connect_status == 0) {
+        printf("Server connected.\n");
+    }
+    else {
+        printf("Can not connect to Server");
+    }
+
+    // after connected to server, client doing initial stuff with server
+    while (1) {
+        // after connected, read instruction from server 
+        read(serv, mess, sizeof(mess));       // read response message from the server
+        // Verify the response to know what to do 
+        if (strcmp(mess, "Please enter username") == 0){
+            printf("Please enter username: ");
+            scanf("%s", mess);
+            write(serv, mess, strlen(mess) + 1);  // send username to server
+        }
+        else if (strcmp(mess, "Please enter password") == 0){
+            printf("Please enter password: ");
+            scanf("%s", mess);
+            // should encrypt password but nah, just do it later
+            write(serv, mess, strlen(mess) + 1);
+        }  // send password to server
+        else if (strcmp(mess, "Wrong password") == 0){
+            printf("Wrong password. Please enter password again. \n");
+            scanf("%s", mess);
+            // encrypt pass can wait to tomorrow
+            write(serv, mess, strlen(mess) + 1);
+        }else if (strcmp(mess, "No username") == 0){
+            printf("Username is not registered. Please enter user name again. \n");
+            scanf("%s", mess);       
+            write(serv, mess, strlen(mess) + 1);
+        }else if (strcmp(mess, "Login success") == 0){
+            printf("Login success !\n");
+            //printf("Connecting to peer...");
+            // Connect to peer base on list given
+            break;
+        }else {
+            // received some wrong mess from server, give 0 f*ck and continue
+            continue;
+        }
+    }
 
 
 //----------------------------------INIT CLIENT wait for incomming peer----------------------------------    
@@ -54,31 +91,34 @@ int main(int argc, char* argv[]) {
     bind(mysocket, (struct sockaddr *)&incomming_addr, inaddr_length);
 
     // then listen
-    listen(mysocket, 5);
+    listen(mysocket, 0);
     printf("Client as server initilize successfully\n");
     
+//----------------------------------STANDBY FOR CONNECTION----------------------------------    
     while (1) {
         peer = accept(mysocket, (struct sockaddr *)&incomming_addr, &inaddr_length);
-        printf("peer %d connected\n",peer);    
+        printf("A Peer (%d) connected\n",peer);    
 
         inet_ntop(AF_INET, &(incomming_addr.sin_addr), peer_ip, sizeof(peer_ip));
         printf("Me: got connection from %s\n", peer_ip);
+
+
     }
 
 
-
+    /*
     // pid_write = fork();
 
     // inet_ntop(AF_INET, get_in_addr((struct sockaddr *)&incomming_addr), s1, sizeof(s1));
-    // printf("client as server: got connection from %s\n", so);
+    // printf("client as server: got connection from %mess\n", so);
 
     // if (pid_write == 0) {
     //     // I'm the son, I'll serve send to this client
     //     while (1) {
-    //         // now it's my (server) turn
+    //         // now it'mess my (server) turn
     //         // keep read mess and send to client
-    //         fgets(s, 95, stdin);
-    //         int numwrite = write(peer, s, strlen(s) + 1);
+    //         fgets(mess, 95, stdin);
+    //         int numwrite = write(peer, mess, strlen(mess) + 1);
     //     }
 
         
@@ -91,11 +131,11 @@ int main(int argc, char* argv[]) {
     //         // but I have the writing thread PID from father
     //         while (1) {
     //             // I wait and read message from client
-    //             int numread = read(peer, s, sizeof(s));
+    //             int numread = read(peer, mess, sizeof(mess));
     //             if (numread != 0)
     //             {
     //                 // read success, print mess
-    //                 printf("client %d says: %s\n",peer,s);    
+    //                 printf("client %d says: %mess\n",peer,mess);    
     //             } else {
     //                 // read fail, disconnected, kill the writing thread.
     //                 printf("client %d disconnected\n", peer);
@@ -127,19 +167,19 @@ int main(int argc, char* argv[]) {
     // // I'm the son, I'm keep reading and sending mess
     //     while (1) {
     //         // read line and send data to server
-    //         fgets(s, 95, stdin);
-    //         int numwrite = write, s, strlen(s) + 1);
+    //         fgets(mess, 95, stdin);
+    //         int numwrite = write, mess, strlen(mess) + 1);
     //     }
     
     // }
     // // I'm the father, I handle reading
     // while(1){
     //     // keep reading
-    //     int numread = read, s, sizeof(s));
+    //     int numread = read, mess, sizeof(mess));
     //     if (numread != 0)
     //     {
     //         // if numread > 0, read success, print mess
-    //         printf("server says: %s\n", s);    
+    //         printf("server says: %mess\n", mess);    
     //     } else {
     //         // read fail, disconnect
     //         printf("server disconnected\n");
@@ -153,5 +193,6 @@ int main(int argc, char* argv[]) {
         
 
     // close);
-    // return 0;
+    */
+    return 0;
 }
